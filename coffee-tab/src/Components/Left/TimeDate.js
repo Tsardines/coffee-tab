896b2b662c19ceb1f8012dd3490ff9c6106a8ea8
Haley Sulcer <hjsulcer@gmail.com>
2018-11-07 14:51:49 -0500

    added date and time---date is 4 days off, however

--- coffee-tab/src/Components/Left/TimeDate.js
+++ coffee-tab/src/Components/Left/TimeDate.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './Left.css';
 
+import Clock from 'react-live-clock';
+
 class TimeDate extends Component {
   render() {
     return (
       <div className="time-date">
 
       <h1>TimeDate Box</h1>
+
+        <div className="time">
+          <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
+        </div>
+
+        <div className="date">
+          <Clock format={'dddd, MMMM Mo, YYYY'} />
+        </div>
 
       </div>
 
