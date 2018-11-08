9c7ec2c69c1f8b9f094b35ded4eb6a3352b48413
Haley <hjsulcer@gmail.com>
2018-11-07 22:00:18 -0500

    removed time aspect of react-live-clock and replaced it...date aspect is still present

--- coffee-tab/src/Components/Left/TimeDate.js
+++ coffee-tab/src/Components/Left/TimeDate.js
@@ -4,22 +4,45 @@ import './Left.css';
 import Clock from 'react-live-clock';
 
 class TimeDate extends Component {
+
+  constructor() {
+    super()
+    this.state={time:new Date()}
+  }
+
+  currentTime()
+  {
+    this.setState({
+      time: new Date()
+    })
+  }
+
+  componentWillMount()
+  {
+    setInterval(() => this.currentTime(),1000)
+  }
+
+
   render() {
     return (
       <div className="time-date">
 
-      <h1>TimeDate Box</h1>
+        <h1>TimeDate Box</h1>
 
-        <div className="time">
-          <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
-        </div>
+      <div className="time">
+
+        <h1>
+          {this.state.time.toLocaleTimeString()}
+        </h1>
+
+      </div>
 
         <div className="date">
           <Clock format={'dddd, MMMM Mo, YYYY'} />
         </div>
 
       </div>
-
+      
     );
   }
 }
